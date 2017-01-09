eb7aab0896b4b29155399ea37d9f7b1d52fae311
Gus <gus.nordhielm@gmail.com>
2017-01-09 12:26:28 -0800

    generate cards with JavaScript

--- starter-code/main.js
+++ starter-code/main.js
@@ -3,14 +3,27 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
-if (cardOne === cardTwo) {
-	alert('You found a match!');
-} else {
-	alert('Sorry, try again.');  
-}
+// if (cardOne === cardTwo) {
+// 	alert('You found a match!');
+// } else {
+// 	alert('Sorry, try again.');  
+// }
 
-if (cardThree === cardFour) {
-	alert('You found a match!');
-} else {
-	alert('Sorry, try again.');  
-}
+// if (cardThree === cardFour) {
+// 	alert('You found a match!');
+// } else {
+// 	alert('Sorry, try again.');  
+// }
+
+var board = document.getElementById('game-board');
+
+var numberOfCards = 4;
+
+var createCards = function() {
+	for (var i = 0; i < numberOfCards; i++) {
+		var newCard = document.createElement('div');
+		newCard.className = 'card';
+		board.appendChild(newCard);
+}};
+
+createCards()
